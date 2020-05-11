d698b8c43d6186bc3e80f5926b53f8e4e8bbac17
Brian <briansangilchung@gmail.com>
2020-05-11 14:52:22 -0400

    commit with enemy pool

---
+++ Assets/Scripts/ObjectPoolScene/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float intervalMin = 0.1f; //min time to span new star
+    public float intervalMax = 0.5f; //max time to span new star
+    public float numEnemies = 1; //how many stars to span at once
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnEnemy", intervalMin); //call SpawnStar after intervalMin time
+    }
+
+    void SpawnEnemy()
+    {
+        //generate numStars stars
+        for (int i = 0; i < numEnemies; i++)
+        {
+            GameObject enemy = EnemyPool.instance.GetEnemy();
+        }
+
+        //call SpawnStar again after a random amount of time between min and max
+        Invoke("SpawnEnemy", Random.Range(intervalMin, intervalMax));
+    }
+}
